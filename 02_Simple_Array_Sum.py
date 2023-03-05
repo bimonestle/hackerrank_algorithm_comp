ae69268527019b96f0dedcf437e2b784239fbc55
bimonestle <bimonestle@yahoo.co.id>
2023-03-05 20:57:38 +0700

    add code alternative

--- 02_Simple_Array_Sum.py
+++ 02_Simple_Array_Sum.py
@@ -1,7 +1,12 @@
 # Given an array of integers, find the sum of its elements.
-# For example, if the array ar = [1,2,3]
+# For example, if the array ar = [1,2,3].
 # 1 + 2 + 3 = 6, so return 6.
 
 def simpleArraySum(ar):
-    # Write your code here
-    return sum(ar)
+    sum = 0
+    for numb in ar:
+        sum += numb
+    return sum
+
+    # Shorter alternatives with built in function
+    # return sum(ar)
