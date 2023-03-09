4de5fcb6cc9601034819a4313e69c8a8ca9397da
bimonestle <bimonestle@yahoo.co.id>
2023-03-09 20:34:25 +0700

    code works fine but buggy in hackerrank

--- 13_Number_Line_Jumps.py
+++ 13_Number_Line_Jumps.py
@@ -1,12 +1,12 @@
 def kangaroo(x1, v1, x2, v2):
-    # print(x1, v1, x2, v2)
-    if v1 <= v2 and x1 <= x2:
-        return 'NO'
-
-    x1 += v1
-    x2 += v2
-    if x1 != x2:
-        kangaroo(x1, v1, x2, v2)
-        print(x1, x2)
     print(x1, x2)
-    return 'YES'
+    if x1 == x2:
+        print('YES')
+        return 'YES'
+    if v1 <= v2 or x1 > x2:
+        print('NO')
+        return 'NO'
+    kangaroo(x1+v1, v1, x2+v2, v2)
+
+kangaroo(0, 3, 4, 2)
+kangaroo(0, 2, 5, 3)
