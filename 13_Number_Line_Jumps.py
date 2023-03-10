8297619e624b2d5c712f58caa891b4cd12ff893b
bimonestle <bimonestle@yahoo.co.id>
2023-03-10 22:01:11 +0700

    solve Number Line Jumps

--- 13_Number_Line_Jumps.py
+++ 13_Number_Line_Jumps.py
@@ -1,10 +1,13 @@
 def kangaroo(x1, v1, x2, v2):
-    print(x1, x2)
-    if x1 == x2:
-        print('YES')
-        return 'YES'
-    elif (x1 < x2 and v1 < v2) or (x1 > x2 and v1 > v2) or (v1 == v2):
-        print('NO')
-        return 'NO'
-    else:
-        return kangaroo(x1+v1, v1, x2+v2, v2)
+    while x1 != x2:
+        if (x1 < x2 and v1 < v2) or (x1 > x2 and v1 > v2) or (v1 == v2):
+            print('NO')
+            return 'NO'
+        x1 += v1
+        x2 += v2
+        print(x1, x2)
+    print('YES')
+    return 'YES'
+    
+kangaroo(2081, 8403, 9107, 8400)
+# kangaroo(21, 6, 47, 3)
