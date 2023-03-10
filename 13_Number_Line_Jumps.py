7fffe3598ff751245ff921f1e60e9f121120f3f9
bimonestle <bimonestle@yahoo.co.id>
2023-03-10 21:36:06 +0700

    cannot pass the test case with big numbers

--- 13_Number_Line_Jumps.py
+++ 13_Number_Line_Jumps.py
@@ -1,8 +1,10 @@
 def kangaroo(x1, v1, x2, v2):
     print(x1, x2)
-    if v1 <= v2 or x1 > x2:
+    if x1 == x2:
+        print('YES')
+        return 'YES'
+    elif (x1 < x2 and v1 < v2) or (x1 > x2 and v1 > v2) or (v1 == v2):
+        print('NO')
         return 'NO'
-    if x1 != x2:
-        kangaroo(x1 + v1, v1, x2 + v2, v2)
-        # print(x1, x2)
-    return 'YES'
+    else:
+        return kangaroo(x1+v1, v1, x2+v2, v2)
