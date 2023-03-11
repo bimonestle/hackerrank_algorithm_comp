60891623f86e8a5712b7bd0b9e967a5d0ba6d280
bimonestle <bimonestle@yahoo.co.id>
2023-03-11 14:52:41 +0700

    solve Between Two Sets

---
+++ 14_Between_Two_Sets.py
@@ -0,0 +1,38 @@
+# There will be two arrays of integers.
+# Determine all integers that satisfy the following two conditions:
+
+# 1. The elements of the first array are all factors of the integer being considered
+# 2. The integer being considered is a factor of all elements of the second array
+
+# These numbers are referred to as being between the two arrays.
+# Determine how many such numbers exist.
+
+# Example:
+# a = [2, 6]
+# b = [24, 36]
+
+# There are two numbers between the arrays: 6 and 12.
+# 6 % 2 == 0, 6 % 6 == 0, 24 % 6 == 0, 36 % 6 == 0.
+# 12 % 2 == 0, 12 % 6 == 0, 12 % 6 == 0, 12 % 6 == 0.
+# return 2.
+
+def getTotalX(a, b):
+    
+    inbetween = []
+    result = []
+    a_lastEl = a[len(a)-1]
+    b_firstEl = b[0]
+    
+    a.sort()
+    b.sort()
+    
+    # get the inbetween numbers
+    for numb in range(a_lastEl, b_firstEl+1):
+        if numb <= b_firstEl:
+            if all(el % numb == 0 for el in b):
+                inbetween.append(numb)
+    for between in inbetween:
+        if all(between % elA == 0 for elA in a):
+            result.append(between)
+            
+    return len(result)
